3337ce547a12616d08c89f9e441920956e9b7405
AlienX420710 <47496458+AlienX420710@users.noreply.github.com>
2026-09-03 21:28:28 -0500

    Set AlienX SmartHome production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://alienxsmarthome.com",
 	integrations: [mdx(), sitemap(), icon()],
 	adapter: cloudflare({
 		platformProxy: {
