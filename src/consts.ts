3a6423babb137dc588be3893b6d454bef594e8f9
AlienX420710 <47496458+AlienX420710@users.noreply.github.com>
2026-09-03 20:24:20 -0500

    Define AlienX SmartHome site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// Site-wide metadata and content constants.
 
 export const SITE_TITLE = "AlienX SmartHome";
-export const SITE_DESCRIPTION = "@_@Spacehead@_@";
+export const SITE_DESCRIPTION =
+	"AlienX SmartHome is an interactive showcase of modern smart home interface design, advanced CSS, JavaScript, accessibility, performance, and web engineering.";
